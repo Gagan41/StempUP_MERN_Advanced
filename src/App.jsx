c7f0eee569929a6e0d7772e28377b315b239b35b
Gagan41 <gagant.p100@gmail.com>
2025-10-31 20:31:13 +0530

    31-10-25

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
+import { useState } from "react";
 import Table from "./Fragments/Table";
+import Modal from "./Fragments/Modal";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
   const tablestyle = {
     border: "5px solid white",
     padding: "10px",
@@ -10,12 +14,47 @@ function App() {
   };
 
   return (
-    <div div className="App">
+    <div className="App">
       <table style={tablestyle}>
         <tbody>
           <Table />
         </tbody>
       </table>
+
+      <h2>Main App component</h2>
+      <button
+        style={{
+          background: "blue",
+          color: "white",
+          padding: "10px 20px",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+        onClick={() => setShowModal(true)}
+      >
+        Show Modal
+      </button>
+
+      {showModal && (
+        <Modal>
+          <h3>This is modal content rendered through a portal</h3>
+          <button
+            style={{
+              marginTop: "20px",
+              padding: "10px 20px",
+              backgroundColor: "red",
+              color: "white",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+            onClick={() => setShowModal(false)}
+          >
+            Close
+          </button>
+        </Modal>
+      )}
     </div>
   );
 }
